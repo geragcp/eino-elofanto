bf4bc090d42ba6cc3bf6d0f25f03124ddeb63dff
geragcp <gero.gcp@gmail.com>
2019-12-14 15:54:28 -0700

    date fix

--- index.js
+++ index.js
@@ -7,13 +7,23 @@ const dateStart = Date.now();
 
 
 app.get('/', (req, res) => {
-    const nDate = new Date().toLocaleString('en-US', {
-        timeZone: 'America/Denver'
-        });
+  const nDate = new Date().toLocaleString('en-US', {
+      timeZone: 'America/Denver'
+      });
+
+  dateObj = new Date(Date.now() - dateStart);
+  hours = dateObj.getUTCHours();
+  minutes = dateObj.getUTCMinutes();
+  seconds = dateObj.getSeconds();
+  
+  timeString = hours.toString().padStart(2, '0') + ':' + 
+      minutes.toString().padStart(2, '0') + ':' + 
+      seconds.toString().padStart(2, '0');
 
   res.json({
     date: nDate,
-    up: `${(Date.now() - dateStart) / 1000} sec.` 
+    up_sec: `${(Date.now() - dateStart) / 1000} sec.`,
+    up_hms: timeString
   });
 });
 
